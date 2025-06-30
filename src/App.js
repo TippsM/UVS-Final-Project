2b051009dae023c367b2e5ef8d6e43ace23224c1
TippsM <mtipp001@fiu.edu>
2025-06-30 16:21:32 -0400

    Edits...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
 import './Styles/App.css'; // this stays the same (unless you move it too)
 import SearchBar from './Components/Searchbar';
 import Navbar from './Components/Navigation-menu';
+import React, { useState } from 'react';
 
 
 function App() {
+
+  const [queries, setQueries] = useState(() => {
+
+    const saved = localStorage.getItem('queries');
+    return saved ? JSON.parse(saved) : [];
+  });
+
   const handleSearch = (query) => {
-    console.log('Searching for:', query);
+
+    const existing = JSON.parse(localStorage.getItem('queries')) || [];
+
+    const updatedQueries = [...existing, query];
+    setQueries(updatedQueries);
+    localStorage.setItem('queries', JSON.stringify(updatedQueries));
+    console.log("All saved queries: ", updatedQueries)
   };
 
   return (
@@ -17,8 +30,15 @@ function App() {
           <h2 className="page-title">Ryder Used Vehicle Inventory</h2>
           <h3 className="page-sub"> Search Used Vehicles </h3>
           <SearchBar onSearch={handleSearch} />
-        </div>
-        
+          <div className="query-history">
+          <h4>Search History:</h4>
+            <ul>
+              {queries.map((query, index) => (
+              <li key={index}>{query}</li>
+              ))}
+           </ul>
+          </div>
+        </div> 
       </header>
     </div>
   );
